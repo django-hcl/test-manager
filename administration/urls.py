41e0d8dae1d24341e19460177c7239441a0d8caa
Vikram G <vikram.g@hcl.com>
2018-03-29 19:10:44 +0530

    usermanagement update

--- administration/urls.py
+++ administration/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
      path('', views.index, name='index'),
      path('login', views.custom_login, name='custom_login'),
+     path('logout', views.logout_function, name='logout_function'),
+     path('user', views.user_list, name='user_list'),
 ]
--- administration/views.py
+++ administration/views.py
@@ -1,14 +1,37 @@
 from django.shortcuts import render, loader
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from .models import Role,Customuser
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def index(request):
+    #return HttpResponseRedirect('/candidate/')
     return render(request,'administration/index.html')
 
 def custom_login(request):
-    print(request)
-    if request.method == "POST":
-        return render(request,'administration/index.html')
+
+    if request.method == 'POST':
+        username = request.POST['username'].strip()
+        password = request.POST['password'].strip()
+        user = authenticate(request,username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('/admin/')
+        else:
+            return HttpResponse('else1')
     else:
-        return HttpResponse("else")
+         return HttpResponseRedirect('/login/')
+        #return render(request,'login.html')
+
+
+def logout_function(request):
+    logout(request)
+    return HttpResponseRedirect('/login/')
+
+
+def user_list(request):
+    user_lists = Customuser.objects.values('custom_userid','custom_userid__username','custom_userid__email','custom_roleid')
+
+    return render(request, 'administration/user_list.html',{'user_lists': user_lists})
