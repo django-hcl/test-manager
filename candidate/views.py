173dbe43433295b4e174a47617ee77de40a23f86
Vikram G <vikram.g@hcl.com>
2018-04-04 18:13:30 +0530

    Candidate views updated

--- candidate/views.py
+++ candidate/views.py
@@ -10,12 +10,18 @@ def index(request):
     return HttpResponseRedirect('/candidate/dashboard')
 def dashboard(request):
     return render(request,'dashboard.html')
+
 def activePage(request):
-    test= Customuser.objects.filter(custom_userid_id=2)
+    current_user = request.user
+    print(current_user.id)
+    test= Customuser.objects.filter(custom_userid_id=current_user.id)
     for val in test:
         value=val.custom_assignedtest
-        value2=Test.objects.filter(test_id=value)
+        val=value.split(",")
+        value2=Test.objects.filter(test_id__in=val)
+
     return render(request,'candidate/active.html',{'tests':value2})
+
 def pending(request):
     return render(request,'candidate/inprogress.html')
 def completed(request):
